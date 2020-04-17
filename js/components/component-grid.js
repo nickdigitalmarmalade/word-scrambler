2b15131f8cd03844ddb26040b1f5ce5772f00394
Nick Rance <nick_rance89@hotmail.com>
2020-04-17 09:53:29 +0100

    fix

--- js/components/component-grid.js
+++ js/components/component-grid.js
@@ -5,38 +5,38 @@ Vue.component('component-grid', {
                 
                     <table>
                         <tr>
-                            <td><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
+                            <td><div>F</div></td>
+                            <td><div>A</div></td>
+                            <td><div>D</div></td>
                             <td class="blank"><div></div></td>
                             <td class="blank"><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
-                            <td class="blank"><div></div></td>
-                        </tr>
-                        <tr>
-                            <td><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
-                            <td class="blank"><div></div></td>
-                            <td class="blank"><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
+                            <td><div>S</div></td>
+                            <td><div>A</div></td>
+                            <td><div>D</div></td>
                             <td class="blank"><div></div></td>
                         </tr>
                         <tr>
-                            <td><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
+                            <td><div>S</div></td>
+                            <td><div>E</div></td>
+                            <td><div>A</div></td>
                             <td class="blank"><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
-                            <td><div></div></td>
+                            <td class="blank"><div></div></td>
+                            <td><div>A</div></td>
+                            <td><div class="is-bonus">D</div></td>
+                            <td><div>S</div></td>
+                            <td class="blank"><div></div></td>
+                        </tr>
+                        <tr>
+                            <td><div>S</div></td>
+                            <td><div>A</div></td>
+                            <td><div>F</div></td>
+                            <td><div>E</div></td>
+                            <td class="blank"><div></div></td>
+                            <td><div>F</div></td>
+                            <td><div class="is-bonus">A</div></td>
+                            <td><div>D</div></td>
+                            <td><div>E</div></td>
+                            <td><div>S</div></td>
                         </tr>
                     </table>
 
