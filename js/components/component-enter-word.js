1f8332fff1e251c98bfa849fca8465e9bbcd7b3c
Nick Rance <nick_rance89@hotmail.com>
2020-04-20 09:32:40 +0100

    enter word component

---
+++ js/components/component-enter-word.js
@@ -0,0 +1,27 @@
+Vue.component('component-enter-word', {
+    template: `<div class="enter-word">
+                
+                 <button @click="enterWord">Enter Word</button>
+
+                </div>`,
+    props: [],
+    data: {
+    	isCorrectWord: false
+    },
+    computed: {
+
+    },
+    methods: {
+		enterWord: function(){
+
+		// is valid word?
+
+
+
+		}
+    },
+    created: function() {
+
+
+    },
+});
